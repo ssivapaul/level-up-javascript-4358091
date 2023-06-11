91ba047fa30f56f2fda57ec872ee9952c606bbba
Siva Sivapalan <81620034+ssivapaul@users.noreply.github.com>
2023-06-11 07:21:10 +0000

    AvailableBooks completed

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,67 @@
 // Write your code here
+/* function Book(title, author, ISBN, numCopies) {   
+    this.title = title;
+    this.author = author;
+    this.isbn = ISBN;
+    this.numCopies = numCopies;
+
+  }
+  Book.prototype.getAvailability = function() {
+    //console.log(this.numCopies);
+    if(this.numCopies <= 0) {
+      return "Out of stock";
+    } else if (this.numCopies  < 10) {
+      return "low stock";
+    } else {
+      return "in stock";  
+    }  
+  }
+
+  Book.prototype.sell = function(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  Book.prototype.restock = function(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+ */
+
+  class Book {
+    constructor(title, author, ISBN, numCopies) {
+      this.title = title;
+      this.author = author;
+      this.ISBN = ISBN;
+      this.numCopies = numCopies;
+    }
+    getAvailability() {
+      console.log(this.numCopies);
+      if(this.numCoipes == 0) {
+        return "Out of stock";
+      } else if ( this.numCopies < 10) {
+        return "Low stock";
+      } else {
+        return "In stock";
+      }
+    }
+
+    sell(numCopiesSold = 1) {
+      this.numCopies -= numCopiesSold;
+    }
+
+    restock(numCopiesStocked = 1) {
+      this.numCopies += numCopiesStocked;
+    }
+    get availability () {
+      return this.getAvailability();
+    }
+  }
+  
+  
+  const HungerGames = new Book("Hunger Gameas", "Suzane Collins", 123919, 5);
+  console.log(HungerGames.getAvailability())
+
+  HungerGames.restock(12);
+  console.log(HungerGames.getAvailability())
+
+  HungerGames.sell(17);
+  console.log(HungerGames.getAvailability())
