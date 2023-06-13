10cd9cdd7b67dae69a45303bf768233404716a4c
Siva Sivapalan <81620034+ssivapaul@users.noreply.github.com>
2023-06-13 00:19:23 +0000

    Private Object completed

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,32 @@
 // Write your code here
+
+// class User {
+//   #username;
+//   #password;
+//   constructor(username, password) {
+//     this.#username = username;
+//     this.#password = password;
+//   }
+//   getUserName() {
+//     return this.#username;
+//   }
+//   getPassWord() {
+//     return this.#password;
+//   }
+// }
+
+// const user = new User("Siva", 1234);
+// console.log(user.getUserName());
+// console.log(user.getPassWord());
+
+const usr = Symbol("userame");
+const pwd = Symbol("passord");
+
+const user = {
+  [usr]: "emmabostian",
+  [pwd]: "1234566",
+  age: 27
+};
+
+console.log(user.usr);
+console.log(user.pwd);
