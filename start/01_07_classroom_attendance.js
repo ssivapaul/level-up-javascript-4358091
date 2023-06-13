e615349bf2c7e2279d4cc2fbaf8fe281fef8518f
Siva Sivapalan <81620034+ssivapaul@users.noreply.github.com>
2023-06-13 10:06:58 +0000

    completed class room

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,20 @@
 // Write your code here
+
+const classRoom = { hasTeachingAsitant: false, classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"] };
+
+  function getStudents(classRoom) {    
+    let { hasTeachingAsitant, classList } = classRoom;
+    let teacher, students, teachingAssistant;
+
+    if(hasTeachingAsitant) {
+      [teacher, teachingAssistant, ...students] = classList;  
+      //console.log(`\nHasTeaching Assistant: ${hasTeachingAsitant}\n\Teaching Assistant: ${teachingAssistant} \n\Students: ${students}\n`);    
+    } else{
+      [teacher, ...students] = classList; 
+      //console.log(`\nHasTeaching Assistant: ${hasTeachingAsitant}\n\Students: ${students}\n`);     
+    }
+    return students;
+  }  
+
+//getStudents(classRoom)
+console.log(getStudents(classRoom));
