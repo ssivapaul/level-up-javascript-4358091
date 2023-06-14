ea57a71db06dd7e69fa3fedf13eef0a5d1ce2763
Siva Sivapalan <81620034+ssivapaul@users.noreply.github.com>
2023-06-14 02:14:27 +0000

    Coffee calculations completed

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,2 +1,12 @@
 // Write your code here
-console.log('hello');
+// const numCoffee = [2, 3, 1, 5];
+// price = 1.25;
+// const sumPrice = numCoffee.reduce((accPrice, curNumCoffee) => accPrice += curNumCoffee * price);
+// console.log(`The total bill is $${sumPrice*price}`);
+
+function coffeeDate(coffeeArr) {
+  const sumPrice = coffeeArr.reduce((accCoffeePrice, currCoffee) => accCoffeePrice + currCoffee);
+  return `The total bill is $${sumPrice*1.25}`;
+}
+
+console.log(coffeeDate([2, 5, 7, 1, 4]));
