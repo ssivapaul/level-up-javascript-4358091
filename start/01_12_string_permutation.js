393bc6a5850b972f655a2bb20eafc902b75ce0a8
Siva Sivapalan <81620034+ssivapaul@users.noreply.github.com>
2023-06-14 12:57:21 +0000

    string permutation completed

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,10 @@
 // Write your code here
+function permutation(letters) {
+  let n = letters.length;
+  if(n === 1) {
+    return 1;
+  }
+  letters = letters.slice(1);
+  return n*permutation(letters);
+}
+console.log(permutation("hel"));
